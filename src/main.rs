ac86bbde5e790a4af34382ca55650c7f8f55670d
Samwel Charles <younggeeks101@gmail.com>
2023-01-20 12:07:48 +0300

    Implement health check tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind address");
+    run(listener)?.await
 }
